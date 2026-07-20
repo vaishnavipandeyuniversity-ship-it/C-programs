1008bd72db5b051714e453de35bcbc8fef539e0d
Vaishnavi Pandey <vaishnavipandeyuniversity@gmail.com>
2026-07-20 23:12:17 +0530

    Update multiplicationtable.c

--- multiplicationtable.c
+++ multiplicationtable.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter a number to see its multiplication table: ");
+    scanf("%d", &num);
+
+    // Loop runs from 1 to 10
+    for (int i = 1; i <= 10; i++) {
+        printf("%d x %d = %d\n", num, i, num * i);
+    }
+
+    return 0;
+}
 
