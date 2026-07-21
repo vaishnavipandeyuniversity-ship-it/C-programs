fcfcc3dfe098a71a510caa3e3fb611d0e57cf947
Vaishnavi Pandey <vaishnavipandeyuniversity@gmail.com>
2026-07-21 23:54:05 +0530

    Update LCM2Num.c

--- LCM2Num.c
+++ LCM2Num.c
@@ -1,1 +1,22 @@
 
+#include <stdio.h>
+
+int main()
+{
+    int x = 15, y = 25, max;
+    max = (x > y) ? x : y;
+
+    // While loop to check if max variable
+    // is divisible by x and y
+    while (1) {
+        if (max % x == 0 && max % y == 0) {
+            printf("The LCM of %d and %d is %d.", x, y,
+                   max);
+            break;
+        }
+
+        ++max;
+    }
+
+    return 0;
+}
