219e29a21887580164ee7838314dc69bcf37ef8c
Vaishnavi Pandey <vaishnavipandeyuniversity@gmail.com>
2026-07-31 23:34:01 +0530

    Update addmatrix.c

--- addmatrix.c
+++ addmatrix.c
@@ -38,3 +38,14 @@ int main() {
 
   return 0;
 }
+#include <stdio.h>
+
+int main() {
+    int arr[5] = {2, 4, 8, 12, 16};
+
+    // Update the first value
+    // of the array
+    arr[0] = 1;
+    printf("%d", arr[0]);
+    return 0;
+}
